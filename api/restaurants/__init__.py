d642a98fce45dd16358898a59ca3bf4971c5bcef
Miles Herr <miles@finexus.com>
2024-03-13 19:01:51 -0500

    change to github updates

--- api/restaurants/__init__.py
+++ api/restaurants/__init__.py
@@ -10,6 +10,6 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 @app.route('/locations')
 def locations():
     locations = [{
-        'id': 1, 'name': 'los tacos numero uno dos tres'
+        'id': 1, 'name': 'los tacos numero uno dos NOW ON GITHUB'
     }]
     return jsonify({'locations': locations})
